af2391d8ede09e1c69d986650b04cad59974ffc5
Cristian Torres <ctorres@codisa.com>
2023-04-03 11:32:44 -0600

    example web pokeApp

--- example-apication-web/app.js
+++ example-apication-web/app.js
@@ -1,51 +1,42 @@
-let items = [];
+const form = document.querySelector('form');
+const input = document.querySelector('#pokemon-name');
+const pokemonImage = document.querySelector('#pokemon-image');
+const pokemonName = document.querySelector('#pokemon-name-info');
+const pokemonType = document.querySelector('#pokemon-type');
+const pokemonAbility = document.querySelector('#pokemon-ability');
+const pokemonHeight = document.querySelector('#pokemon-height');
+const pokemonWeight = document.querySelector('#pokemon-weight');
 
-const renderItems = () => {
-	const list = document.querySelector('#items');
-	list.innerHTML = '';
-	items.forEach((item, index) => {
-		const li = document.createElement('li');
-		li.innerHTML = `
-      <span>${item.text}</span>
-      <button data-index="${index}">x</button>
-    `;
-		if (item.completed) {
-			li.classList.add('completed');
+const fetchPokemonData = async (pokemonName) => {
+	try {
+		const response = await fetch(
+			`https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+		);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+const displayPokemonData = (pokemonData) => {
+	pokemonImage.src = pokemonData.sprites.front_default;
+	pokemonName.textContent = pokemonData.name;
+	pokemonType.textContent = `Type: ${pokemonData.types[0].type.name}`;
+	pokemonAbility.textContent = `Ability: ${pokemonData.abilities[0].ability.name}`;
+	pokemonHeight.textContent = `Height: ${pokemonData.height} m`;
+	pokemonWeight.textContent = `Weight: ${pokemonData.weight} kg`;
+};
+
+form.addEventListener('submit', async (event) => {
+	event.preventDefault();
+	const pokemonNameValue = input.value.trim().toLowerCase();
+	if (pokemonNameValue !== '') {
+		const pokemonData = await fetchPokemonData(pokemonNameValue);
+
+		if (pokemonData) {
+			displayPokemonData(pokemonData);
+			input.value = '';
 		}
-		list.appendChild(li);
-	});
-};
-
-const addItem = (event) => {
-	event.preventDefault();
-	const input = document.querySelector('#new-item');
-	const text = input.value.trim();
-	if (text) {
-		items.push({ text, completed: false });
-		input.value = '';
-		renderItems();
 	}
-};
-
-const removeItem = (event) => {
-	event.preventDefault();
-	items = items.filter((item) => !item.completed);
-	renderItems();
-};
-
-const toggleItem = (event) => {
-	const index = event.target.dataset.index;
-	if (index !== undefined) {
-		items[index].completed = !items[index].completed;
-		renderItems();
-	}
-};
-
-const addButton = document.querySelector('#add-btn');
-addButton.addEventListener('click', addItem);
-
-const removeButton = document.querySelector('#remove-btn');
-removeButton.addEventListener('click', removeItem);
-
-const list = document.querySelector('#items');
-list.addEventListener('click', toggleItem);
+});
