84f361a6bb67d411eb713beeca2acc96ab16b555
Cristian Torres <ctorres@codisa.com>
2023-04-07 16:33:39 -0600

    ejemplos razor pages

---
+++ 12-RazorRoutes/12-RazorRoutes/Pages/Product/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _12_RazorRoutes.Pages.Product
+{
+    public class DetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
