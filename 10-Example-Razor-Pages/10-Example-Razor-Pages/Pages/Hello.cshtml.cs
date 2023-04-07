0bbf3ddcb18043afec18cbed012a8dfa546c0b93
Cristian Torres <ctorres@codisa.com>
2023-04-07 11:56:18 -0600

    ejercicios razor pages

---
+++ 10-Example-Razor-Pages/10-Example-Razor-Pages/Pages/Hello.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _10_Example_Razor_Pages.Pages
+{
+    public class HelloModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
