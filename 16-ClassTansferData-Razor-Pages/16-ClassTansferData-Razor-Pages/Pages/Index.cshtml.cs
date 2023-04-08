2138274c4b484dd9174f357a9115eca1d30a5edb
Cristian Torres <ctorres@codisa.com>
2023-04-08 12:58:48 -0600

    ejercicios razor pages

---
+++ 16-ClassTansferData-Razor-Pages/16-ClassTansferData-Razor-Pages/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _16_ClassTansferData_Razor_Pages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [ViewData]
+        public string Message { get; set; }
+        [BindProperty]
+        public string Name { get; set; }
+        public void OnGet()
+        {
+            ViewData["MyString"] = "Hola";
+            ViewData["MyNumber"] = 100;
+
+            Message = "Hello Word";
+
+        }
+
+        public IActionResult OnPost()
+        {
+            // Almacenar mensaje en TempData para mostrar en la página siguiente
+            TempData["Name"] = Name;
+            // Redirigir a la página siguiente
+            return RedirectToPage("/Privacy");
+        }
+    }
+}
