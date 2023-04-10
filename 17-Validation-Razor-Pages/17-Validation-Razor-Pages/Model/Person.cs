c6fec667cee02033bdd2471ce167584fc9f146be
Cristian Torres <ctorres@codisa.com>
2023-04-10 07:31:29 -0600

    Ejercicios razor pages

---
+++ 17-Validation-Razor-Pages/17-Validation-Razor-Pages/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _17_Validation_Razor_Pages.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [StringLength(25, ErrorMessage = "{0} La longitud debe estar entre {2} y {1}.", MinimumLength = 3)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Solo se permiten letras")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [Range(18, 100, ErrorMessage = "Solo se permiten mayores de edad")]
+        public int? Age { get; set; }
+    }
+}
