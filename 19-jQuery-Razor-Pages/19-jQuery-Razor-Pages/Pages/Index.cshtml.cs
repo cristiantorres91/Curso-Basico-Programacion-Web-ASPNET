e900eb597b68bebb079a665441de3dde4041a587
Cristian Torres <ctorres@codisa.com>
2023-04-11 08:19:28 -0600

    ejercicios razor pages

---
+++ 19-jQuery-Razor-Pages/19-jQuery-Razor-Pages/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _19_jQuery_Razor_Pages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Name { get; set; }
+        public void OnGet()
+        {
+            Name = "Mágico González ";
+        }
+    }
+}
