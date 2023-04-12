fe7937175dd10c32e958fd6b636f9b0af29cf3b3
Cristian Torres <ctorres@codisa.com>
2023-04-12 06:43:46 -0600

    ejercicios razor

---
+++ 22-AJAX-Razor-Pages/22-AJAX-Razor-Pages/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _22_AJAX_Razor_Pages.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public void OnGet()
+        {
+
+        }
+
+        public JsonResult OnGetData()
+        {
+            var data = new { name = "Juan", age = 25 };
+            return new JsonResult(data);
+        }
+    }
+}
---
+++ 22-AJAX-Razor-Pages/22-AJAX-Razor-Pages/Pages/Privacy.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _22_AJAX_Razor_Pages.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        [BindProperty]
+        public decimal Num1 { get; set; }
+
+        [BindProperty]
+        public decimal Num2 { get; set; }
+        [BindProperty]
+        public string Operation { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public JsonResult OnGetResult(decimal num1, decimal num2, string operation)
+        {
+            var result = Calculate(num1, num2, operation);
+            return new JsonResult(result);
+        }
+
+        private decimal Calculate(decimal num1, decimal num2, string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return num1 + num2;
+                case "-":
+                    return num1 - num2;
+                case "*":
+                    return num1 * num2;
+                case "/":
+                    return num1 / num2;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
