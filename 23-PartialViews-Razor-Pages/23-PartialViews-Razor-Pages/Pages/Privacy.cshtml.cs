30912be8ec1b1ee15561ca74c84a66f0243bc864
Cristian Torres <ctorres@codisa.com>
2023-04-12 09:22:36 -0600

    ejemplos razor pages

---
+++ 23-PartialViews-Razor-Pages/23-PartialViews-Razor-Pages/Pages/Privacy.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _23_PartialViews_Razor_Pages.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public List<string> Comments = new List<string>();
+        public IActionResult OnGetComments()
+        {
+            Comments.AddRange(new[] { "Comentario 1", "Comentario 2", "Comentario 3" });
+            return Partial("_CommentsProduct", Comments);
+        }
+    }
+}
